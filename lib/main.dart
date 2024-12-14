8de143800d7a192c8c23a9cbef9b6bfb894617a9
Gabbygabijo <gabrielfashedemi@gmail.com>
2024-12-14 03:52:57 +0100

    The main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
+import 'package:aflutterapp/data_service.dart';
 import 'package:flutter/material.dart';
-import 'package:screenshot/screenshot.dart';
-import 'package:share_plus/share_plus.dart';
-import 'dart:io'; // Import dart:io for file operations 
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,50 +13,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _screenshotController = ScreenshotController();
+  final _dataService = DataService();
+  String? _response;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Screenshot(
-              controller: _screenshotController,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Image.asset('images/codepassionately.png'),
-                    const Text(
-                      'Code Passionately',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            TextButton(
-                onPressed: _takeScreenshot,
-                child: const Text('Take Screenshot and share'))
-          ],
-        )),
+          child: Builder(builder: (context) {
+            if (_response != null) {
+              return Text(_response!);
+            } else {
+              return ElevatedButton(
+                  onPressed: _makeRequest, child: const Text('Make Request'));
+            }
+          }),
+        ),
       ),
     );
   }
 
-  void _takeScreenshot() async {
-    final imageFile = await _screenshotController.capture();
-    if (imageFile != null) {
-      final tempDir = await getTemporaryDirectory();
-      final filePath = '${tempDir.path}/screenshot.png';
-      final file = await File(filePath).writeAsBytes(imageFile);
-      Share.shareXFiles([XFile(file.path)], text: 'Check out this screenshot!');
-    } else {
-      print('Screenshot failed');
-    }
+  void _makeRequest() async {
+    final response = await _dataService.makeRequestToApi();
+    setState(() {
+      _response = response;
+    });
   }
 }
