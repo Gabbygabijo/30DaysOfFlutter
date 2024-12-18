6e64bf40df3d4d56510b4d0da2b428723800f6b7
Gabbygabijo <gabrielfashedemi@gmail.com>
2024-12-18 03:15:32 +0100

    The main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aflutterapp/data_service.dart';
+import 'package:aflutterapp/models.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _cityTextController = TextEditingController();
   final _dataService = DataService();
+  WeatherResponse? _response;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +26,28 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(padding: const EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                width: 150,
-                child: TextField(
-                  controller: _cityTextController,
-                  decoration: const InputDecoration(labelText: 'City'),
-                  textAlign: TextAlign.center,
+              if (_response != null)
+                Column(
+                  children: [
+                    Image.network(_response!.iconUrl),
+                    Text(
+                      '${_response?.tempInfo?.temperature}º',
+                      style: const TextStyle(fontSize: 40),
+                    ),
+                    Text('${_response?.weatherInfo?.description}'),
+                  ],
                 ),
-              ),),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    controller: _cityTextController,
+                    decoration: const InputDecoration(labelText: 'City'),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
               ElevatedButton(onPressed: _search, child: const Text('Search'))
             ],
           ),
@@ -40,8 +55,9 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-  
-  void _search(){
-    _dataService.getWeather(_cityTextController.text);
+
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    setState(() => _response = response);
   }
 }
