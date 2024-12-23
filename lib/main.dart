3513bb9b08bb9d3611253b0ea30f42564a1b5e25
Gabbygabijo <gabrielfashedemi@gmail.com>
2024-12-23 15:02:21 +0100

    Main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aflutterapp/posts_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,47 +13,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _users = List.filled(20, 'Gabby');
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          backgroundColor: Colors.white70,
-          appBar: AppBar(
-            bottom: const TabBar(tabs: [Text('List'), Text('Grid')]),
-            backgroundColor: Colors.white30,
-            // foregroundColor: Colors.white24,
-          ),
-          body: TabBarView(
-            children: [_contentListView(), _constGridView()],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _contentListView() {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) => Card(
-        child: ListTile(
-          title: Text(index.toString()),
-          onTap: () => print('Hello $index'),
-        ),
-      ),
-    );
-  }
-
-  Widget _constGridView() {
-    return GridView.builder(
-      itemCount: _users.length,
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) =>
-          Card(child: GridTile(child: Center(child: Text(_users[index])))),
+    return const MaterialApp(
+      home: PostsView(),
     );
   }
 }
