6f4735807a9243e2e202958d416a8ca4e00ad412
Gabbygabijo <gabrielfashedemi@gmail.com>
2025-01-08 22:58:55 +0100

    Added an onTap event

--- lib/posts_view.dart
+++ lib/posts_view.dart
@@ -1,3 +1,4 @@
+import 'package:aflutterapp/nav_cubit.dart';
 import 'package:aflutterapp/posts_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,6 +33,7 @@ class PostsView extends StatelessWidget {
                 return Card(
                   child: ListTile(
                     title: Text(state.posts[index].title),
+                    onTap: () => BlocProvider.of<NavCubit>(context).showPostDetails(state.posts[index]),
                   ),
                 );
               }),
