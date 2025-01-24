526da971084491754d6ffc64f08ff03acd98a617
Gabbygabijo <gabrielfashedemi@gmail.com>
2025-01-25 00:22:25 +0100

    Update README and implement PokedexView as the main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:aflutterapp/app_navigator.dart';
-import 'package:aflutterapp/data_service.dart';
-import 'package:aflutterapp/nav_cubit.dart';
-import 'package:aflutterapp/posts_cubit.dart';
+import 'package:aflutterapp/pokedex_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,12 +16,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MultiBlocProvider(providers: [
-        BlocProvider(create: (context) => NavCubit()),
-        BlocProvider(
-          create: (context) => PostsBloc(DataService())..add(LoadPostsEvent()),
-        )
-      ], child: AppNavigator()),
+      theme: Theme.of(context).copyWith(primaryColor: Colors.red, colorScheme: Theme.of(context).colorScheme.copyWith(secondary: Colors.redAccent)),
+      home: PokedexView(),
     );
   }
 }
